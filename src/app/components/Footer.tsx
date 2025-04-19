3e402e5a033d45fb6f11518244142e8879d39907
Anders Flatland Myrvang <andersfm@stud.ntnu.no>
2025-04-19 17:36:32 +0200

    Fix LinkedIn URL in footer component

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -25,7 +25,7 @@ export default function Footer() {
             </svg>
           </a>
 
-          <a href="https://www.linkedin.com/in/anders-myrvang-2b52092b3/">
+          <a href="https://www.linkedin.com/in/andersmyrvang/">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="100"
